e3db8b044095b6e8b4c32e9af1ac08c052bb59d3
pablo <pablo@DESKTOP-2A346VJ>
2023-02-15 12:06:10 +0100

    base

--- Juego.py
+++ Juego.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+from Laberinto import Laberinto
+from Habitacion import Habitacion
+from Puerta import Puerta
+from Pared import Pared
+
 class Juego:
-    pass
+    def__init__(self):
+	self.laberinto=None
+    def laberinto2habitaciones(self):
+	self.laberinto=Laberinto()
+	
+	hab1=Habitacion(1)
+	hab2=Habitacion(2)
+	
+	puerta=Puerta()
+	puerta.lado1=hab1
+	puerta.lado2=hab2
+
+	hab1.norte=Pared()
+	hab1.este=Pared()
+	hab1.oeste=Pared()
+
+	hab2.sur=Pared()
+	hab2.este=Pared()
+	hab2.oeste=Pared()
+
+	self.laberinto.agregarHabitacion(hab1)
+	self.laberinto.agregarHabitacion(hab2)´
+
+juego=Juego()
+juego.laberinto2Habitaciones()
+	
--- Laberinto.py
+++ Laberinto.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+from Habitacion import Habitacion
 class Laberinto:
-    pass
+    def __init__(self):
+	self.habitaciones=list()
+    def agregarHabitacion(self,hab):
+	self.habitaciones
